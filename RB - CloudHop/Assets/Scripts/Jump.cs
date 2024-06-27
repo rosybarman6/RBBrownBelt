c279696e1bdda7a8316cf6aa3da4c9cf659dc81e
MasterHacker10 <rosybarman6@gmail.com>
2024-06-27 17:02:38 -0400

    FINISHED CLOUD HOP
    PROVES THE OPPS WRONG

--- RB - CloudHop/Assets/Scripts/Jump.cs
+++ RB - CloudHop/Assets/Scripts/Jump.cs
@@ -6,16 +6,30 @@ public class Jump : MonoBehaviour
 {
     Rigidbody2D rigidbody;
     float jumpForce = 15;
+    public bool canJump;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
-        rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(rigidbody.velocity.y > -.01 && rigidbody.velocity.y < .01)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+        if (canJump && Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
         
     }
 }
