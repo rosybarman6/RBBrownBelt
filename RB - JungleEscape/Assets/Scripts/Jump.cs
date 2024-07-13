0e72e33a635d01b5b9663cfe093d388b0cf8161a
MasterHacker10 <rosybarman6@gmail.com>
2024-07-13 12:55:12 -0400

    JungleEscape
    fdsfdsafdsa

--- RB - JungleEscape/Assets/Scripts/Jump.cs
+++ RB - JungleEscape/Assets/Scripts/Jump.cs
@@ -8,15 +8,24 @@ public class Jump : MonoBehaviour
 
     float jumpForce = 5.7f;
 
+    public bool isGrounded;
+
+    float fallMultiplier = 1.5f;
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>(); 
     }
 
     void Update()
     {
-        if(Input.GetButtonDown("Jump")){
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, .15f);
+        Debug.DrawRay(transform.position, Vector3.down * .15f, Color.red);
+        if(Input.GetButtonDown("Jump") && isGrounded){
             rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+        if(rigidbody.velocity.y < 0)
+        {
+            rigidbody.velocity += Physics.gravity * fallMultiplier * Time.deltaTime;
         }
     }
 }
