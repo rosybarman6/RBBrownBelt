7a887646a527ef9032351b8d8b16945055790e19
MasterHacker10 <rosybarman6@gmail.com>
2024-07-20 13:44:45 -0400

    wip 7/20

---
+++ RB - JungleEscapePY/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    public Transform Player;
+    
+    void Update()
+    {
+       bool hit = Physics.Raycast(transform.position, transform.forward, 3f, 1 << 8);
+
+        if (hit)
+        {
+            Debug.Log("CAUTIOUN, PRECAUCIÓN");
+        }
+        else
+        {
+            Debug.Log("ALL CLEAR, TODO CLARO");
+        }
+    }
+}
