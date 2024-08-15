8d5e286375d849f7efa4cfb366b75d4ba548e846
MasterHacker10 <rosybarman6@gmail.com>
2024-08-15 16:50:35 -0400

    ROBOMANIA
    DACDSWAVRDEWQVRDWQVRDQVRFEAQBVRFQBVRDEQBVRF

--- RB - RoboMania/Assets/Scripts/EnemyMovement.cs
+++ RB - RoboMania/Assets/Scripts/EnemyMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float yForce;
+    public float xForce;
+    public float xDirection;
     private Rigidbody2D enemyRigidbody;
-    public float speed;
+    
 
     void Start()
     {
@@ -16,20 +18,23 @@ public class EnemyMovement : MonoBehaviour
     {
         if(transform.position.x <= -8)
         {
-            
+            xDirection = 1;
+            enemyRigidbody.AddForce(Vector2.right * xForce);
         }
         if (transform.position.x >= 8)
         {
-            
+            xDirection = -1;
+            enemyRigidbody.AddForce(Vector2.left * xForce);
         }
         
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collison.gameObject.tag == "Ground")
+        if(collision.gameObject.tag == "Ground")
         {
-
+            Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+            enemyRigidbody.AddForce(jumpForce);
         }
     }
 }
