0ab93e99cae84ee805958fc4d34dcfff3727c040
MasterHacker10 <141876965+MasterHacker10@users.noreply.github.com>
2024-09-25 15:43:09 -0400

    CONTINUES CYBERFU2
    CSC

--- RB - CyberFu2/Assets/Scripts/PlayerHealth.cs
+++ RB - CyberFu2/Assets/Scripts/PlayerHealth.cs
@@ -6,10 +6,16 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     public int currentPlayerHealth = 10;
+    public int enemyDamage = 2;
    
     void Start()
     {
        
     }
     
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage;
+
+    }
 }
