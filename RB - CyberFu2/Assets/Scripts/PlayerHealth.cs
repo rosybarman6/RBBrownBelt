9b14428eb0038da8ecbf598c300fa3a5e8928bb2
MasterHacker10 <rosybarman6@gmail.com>
2024-09-27 18:39:15 -0400

    CYBERFU2
    CDSADSAFDSAFDSASA

--- RB - CyberFu2/Assets/Scripts/PlayerHealth.cs
+++ RB - CyberFu2/Assets/Scripts/PlayerHealth.cs
@@ -7,15 +7,38 @@ public class PlayerHealth : MonoBehaviour
 {
     public int currentPlayerHealth = 10;
     public int enemyDamage = 2;
+    public PlayerExplosionParticles particles;
+    private Animator playerAnimator;
    
     void Start()
     {
-       
+        playerAnimator = GetComponent<Animator>();
+        particles = GetComponent<PlayerExplosionParticles>();
     }
     
     public void HurtPlayer()
     {
         currentPlayerHealth -= enemyDamage;
+        playerAnimator.SetTrigger("Hit");
 
+        if(currentPlayerHealth <= 0)
+        {
+            particles.Explode();
+            Invoke("ReloadScene", 5);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "HitCollider")
+        {
+            HurtPlayer();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("CyberFu");
     }
 }
