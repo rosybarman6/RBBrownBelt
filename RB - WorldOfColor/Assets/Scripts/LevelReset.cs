f178e8f45eed2cf01c19b892c64a9b916dc07a02
MasterHacker10 <141876965+MasterHacker10@users.noreply.github.com>
2024-10-16 16:47:51 -0400

    WorldOfColorPY started
    fsadsadsadsa

--- RB - WorldOfColor/Assets/Scripts/LevelReset.cs
+++ RB - WorldOfColor/Assets/Scripts/LevelReset.cs
@@ -5,17 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class LevelReset : MonoBehaviour
 {
+    public ParticleSystem trail;
+    public ParticleSystem explosion;
     public GameObject player;
+
+    void Start()
+    {
+        explosion.Stop();
+        trail.Play();
+
+    }
+
+    void FixedUpdate()
+    {
+        explosion.transform.position = player.transform.position;
+        trail.transform.position = player.transform.position;
+
+    }
     public void GameOver()
     {
         player.SetActive(false);
-        Invoke("Reload", 2);
-        
+       Invoke("Reload", 2);
+        explosion.Play();
+        trail.Stop();
+       
+
     }
 
     void Reload()
     {
-        SceneManager.LoadScene(0);
+       SceneManager.LoadScene(0);
     }
 }
 
