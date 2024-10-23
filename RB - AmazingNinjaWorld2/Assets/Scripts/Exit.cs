bf358496b45ea44423d746416089f5d584e37763
MasterHacker10 <141876965+MasterHacker10@users.noreply.github.com>
2024-10-23 16:49:22 -0400

    finished anw2
    dsadsadsadsadsadsadsads

---
+++ RB - AmazingNinjaWorld2/Assets/Scripts/Exit.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public GameObject gem;
+    public GameObject background;
+    public string teleportDestination;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(gem.activeInHierarchy == false)
+        {
+            background.GetComponent<GameManager>().TeleportOpen(teleportDestination);
+        }
+    }
+}
